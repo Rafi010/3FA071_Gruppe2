d17752baad75e39281132166851841af7f35c5d4
Fabi <fabian.alletter@tutanota.de>
2024-10-11 08:50:58 +0200

    Properties file and Reader

---
+++ src/main/java/dev/hv/test/Util.java
@@ -0,0 +1,37 @@
+package dev.hv.test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Util {
+
+    public static void main(String [] args){
+        getConnection("hv");
+    }
+
+
+    private static Connection con = null;
+
+    public static void getConnection(final String db){
+        if (con == null){
+            try{
+                final Properties prop = new Properties();
+                final String home = System.getProperty("user.home");
+                prop.load(new FileReader(db + ".properties"));
+                final String dburl = prop.getProperty("DBURL");
+                final String dbuser = prop.getProperty("DBUSER");
+                final String dbpw = prop.getProperty("DBPW");
+                System.out.println(home);
+
+                con = DriverManager.getConnection(dburl, dbuser, dbpw);
+            }catch (SQLException | IOException e){
+                throw new RuntimeException(e);
+            }
+        }
+        //return con;
+    }
+}
