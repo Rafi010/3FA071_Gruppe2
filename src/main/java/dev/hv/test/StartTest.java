87e9645028c3ffee6a341f8467d7f62a44e64d8c
Raphael Rauch <raphael.rauch123@gmail.com>
2024-10-15 08:26:47 +0200

    prop file creation, readout in util

--- src/main/java/dev/hv/test/StartTest.java
+++ src/main/java/dev/hv/test/StartTest.java
@@ -2,7 +2,10 @@ package dev.hv.test;
 
 public class StartTest {
    public static void main(String[] args) {
+      CreateProperties createProp = new CreateProperties();
       DatabaseConnection connection = new DatabaseConnection();
+
+      createProp.Start();
       connection.openConnection(null);
       connection.createAllTables();
    }
