98e400f4aa8d76f2e179b7d28a9f6ebc30006ec7
Raphael Rauch <raphael.rauch123@gmail.com>
2024-10-17 08:10:54 +0200

    removed 2 unused imports

--- src/main/java/dev/hv/dbComm/sqlExecuter.java
+++ src/main/java/dev/hv/dbComm/sqlExecuter.java
@@ -3,9 +3,6 @@ package dev.hv.dbComm;
 
 import org.mariadb.jdbc.Connection;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class sqlExecuter {
 
     public void execute(Connection con, String filePath){
