55ec620742da29735db53d25f415260c4ece44f1
Hazim <147995561+Hazim-Mehmedovic@users.noreply.github.com>
2024-10-18 10:55:14 +0200

    Completion of SQL queries for uploading CSV files.

--- dateien/sql/create_table.sql
+++ dateien/sql/create_table.sql
@@ -2,31 +2,27 @@ CREATE TABLE IF NOT EXISTS heizung (
     kundenid VARCHAR(255),
     zaehlernummer VARCHAR(255),
     datum DATE,
-    zaehlerstand FLOAT
+    zaehlerstand_in_mwh FLOAT,
+    kommentar VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS wasser (
     kundenid VARCHAR(255),
     zaehlernummer VARCHAR(255),
     datum DATE,
-    zaehlerstand FLOAT
+    zaehlerstand_in_m³ FLOAT,
+    kommentar VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS strom (
     kundenid VARCHAR(255),
     zaehlernummer VARCHAR(255),
     datum DATE,
-    zaehlerstand FLOAT
+    zaehlerstand_in_kwh FLOAT,
+    kommentar VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS kunde (
-    uuid VARCHAR(255),
-    anrede VARCHAR(255),
-    nachname VARCHAR(255),
-    vorname VARCHAR(255),
-    geburtsdatum DATE
-);
-CREATE TABLE IF NOT EXISTS test_kunde (
     uuid VARCHAR(255),
     anrede VARCHAR(255),
     nachname VARCHAR(255),
