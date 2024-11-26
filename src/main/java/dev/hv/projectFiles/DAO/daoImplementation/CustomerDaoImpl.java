3936be96f3adc43eb818e3aa3bb161a38996f5bb
Raphael Rauch <raphael.rauch123@gmail.com>
2024-11-26 13:47:19 +0100

    first documentation dao

--- src/main/java/dev/hv/projectFiles/DAO/daoImplementation/CustomerDaoImpl.java
+++ src/main/java/dev/hv/projectFiles/DAO/daoImplementation/CustomerDaoImpl.java
@@ -18,6 +18,10 @@ private Connection connection;
         this.connection = connection;
     }
 
+    /**
+     * funktion um einen Nutzer in der Datenbank zu erstellen
+     * @param user das nutzer Objekt welches in die DB eingefügt wird
+     */
     @Override
     public void addUser(User user) {
 
@@ -38,12 +42,21 @@ private Connection connection;
         }
     }
 
+    /**
+     * Funktion um einen Benutzer aus der Datenbank anhand der ID zu bekommen
+     * @param id id anhand welcher der nutzer/user gesucht wird
+     * @return gibt den anhand der db daten erstellten nutzer zurück
+     */
     @Override
     public ICustomer getUserById(String id) {
         try {
+            //query vorlage
             String query = "SELECT * FROM users WHERE uuid = ?";
+            //query in ein preparedStatement speichern
             PreparedStatement stmt = connection.prepareStatement(query);
+            //fragezeichen in der query anhand vom index mit daten ersetzen
             stmt.setString(1, id);
+            //ergebnisse der query in ein ResultSet speichern
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
@@ -61,6 +74,10 @@ private Connection connection;
         return null;
     }
 
+    /**
+     * Funktion um alle Nutzer aus der DB auszugeben in einer liste
+     * @return Liste mit den nutzer/user Objekten welche zuvor für jeden Eintrag erstellt wurden
+     */
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
@@ -84,6 +101,10 @@ private Connection connection;
         return users;
     }
 
+    /**
+     * Funktion um einen nutzer aus der DB zu entfernen
+     * @param id die ID des nutzers welcher aus der DB entfernt wird
+     */
     @Override
     public void deleteUser(String id) {
         try {
@@ -96,6 +117,10 @@ private Connection connection;
         }
     }
 
+    /**
+     * Funktion um einen nutzer innerhalb der DB zu updaten
+     * @param user das nutzer/user Objekt welches updated werden soll1
+     */
     @Override
     public void updateUser(User user) {
 
