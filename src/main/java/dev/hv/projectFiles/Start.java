bf127dd0023221f9ce3ae51afcf5f2ed63383a8b
Fabi <fabian.alletter@tutanota.de>
2024-11-26 14:23:03 +0100

    fixed properties parameter and removed unused imports in Start.java

--- src/main/java/dev/hv/projectFiles/Start.java
+++ src/main/java/dev/hv/projectFiles/Start.java
@@ -1,18 +1,12 @@
 package dev.hv.projectFiles;
 
-import dev.hv.model.ICustomer;
-import dev.hv.model.IReading;
 import dev.hv.projectFiles.DAO.daoImplementation.CustomerDaoImpl;
 import dev.hv.projectFiles.DAO.daoImplementation.ReadingDaoImpl;
 import dev.hv.projectFiles.DAO.daoInterfaces.CustomerDao;
 import dev.hv.projectFiles.DAO.daoInterfaces.ReadingDao;
-import dev.hv.projectFiles.DAO.entities.Reading;
-import dev.hv.projectFiles.DAO.entities.User;
 import dev.hv.rest.Server;
 
-import java.time.LocalDate;
 import java.util.Properties;
-import java.util.UUID;
 
 public class Start {
    public static void main(String[] args) {
@@ -21,6 +15,8 @@ public class Start {
          port = "8080";  // default port for local development
       }
       String baseUri = "http://0.0.0.0:" + port + "/";
+      //create properties object for use as parameter
+      Properties properties = new Properties();
       //create createProp object to create the properties file
       CreateProperties createProp = new CreateProperties();
       //create connection object used for database communication
@@ -29,11 +25,12 @@ public class Start {
       //create the properties file
       createProp.Create();
       //open the general connection to mySql
-      connection.openConnection();
+      connection.openConnection(properties);
       //create the hv database
       connection.createDatabase();
       //close the old connection and open the new one with the hv database
       connection.closeConnection();
+
       connection.openHvConnection(properties);
       //create the tables and fill them
       connection.createAllTables();
