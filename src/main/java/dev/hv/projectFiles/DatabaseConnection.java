f69c0194a6f8e6547e32df9f7ee06caf88055c05
Raphael Rauch <raphael.rauch123@gmail.com>
2024-11-27 14:25:57 +0100

    change in db_url getter

--- src/main/java/dev/hv/projectFiles/DatabaseConnection.java
+++ src/main/java/dev/hv/projectFiles/DatabaseConnection.java
@@ -22,7 +22,7 @@ public class DatabaseConnection implements IDatebaseConnection {
 //                System.out.println("No .env file found. Proceeding with environment variables.");
 //            }
 //            String dbUrl = dotenv != null ? dotenv.get("MYSQL_URL") : "mysql://root:YjltZVfpfIyxhXVDkluArCKdzvxffIgl@mysql.railway.internal:3306/railway";
-            this.connection = DriverManager.getConnection("jdbc:mysql://mysql.railway.internal:3306/railway)", "root", "YjltZVfpfIyxhXVDkluArCKdzvxffIgl");
+            this.connection = DriverManager.getConnection("jdbc:mysql://mysql.railway.internal:3306/railway", "root", "YjltZVfpfIyxhXVDkluArCKdzvxffIgl");
             System.out.println("Connected to MySql");
         } catch (SQLException e) {
             throw new RuntimeException(e);
