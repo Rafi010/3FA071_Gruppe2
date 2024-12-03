5b218f5a08fcbb0611e5002319dcb860a5a3e1b0
Raphael Rauch <raphael.rauch123@gmail.com>
2024-12-03 10:20:35 +0100

    change in create_table.sql to set id's as unique and set fields that should not be null to NOT NULL

--- dateien/sql/create_table.sql
+++ dateien/sql/create_table.sql
@@ -1,32 +1,32 @@
 --Erstellt die Tabelle Heizung mit den Spalten KundenID, Zaehlernummer, Datum, Zaehlernummer_in_mhw und Kommentar.
 CREATE TABLE IF NOT EXISTS heizung (
-    kundenid VARCHAR(255),
-    zaehlernummer VARCHAR(255),
-    datum DATE,
-    zaehlerstand_in_mwh FLOAT,
-    kommentar VARCHAR(255)
+    kundenid VARCHAR(255) NOT NULL,
+    zaehlernummer VARCHAR(255) NOT NULL UNIQUE,
+    datum DATE NOT NULL,
+    zaehlerstand_in_mwh FLOAT NOT NULL,
+    kommentar VARCHAR(255) NOT NULL
 );
 --Erstellt die Tabelle Wasser mit den Spalten KundenID, Zaehlernummer, Datum, Zaehlernummer_in_m³ und Kommentar.
 CREATE TABLE IF NOT EXISTS wasser (
-    kundenid VARCHAR(255),
-    zaehlernummer VARCHAR(255),
-    datum DATE,
-    zaehlerstand_in_m3 FLOAT,
-    kommentar VARCHAR(255)
+    kundenid VARCHAR(255) NOT NULL,
+    zaehlernummer VARCHAR(255) NOT NULL UNIQUE,
+    datum DATE NOT NULL,
+    zaehlerstand_in_m3 FLOAT NOT NULL,
+    kommentar VARCHAR(255) NOT NULL
 );
 --Erstellt die Tabelle Strom mit den Spalten KundenID, Zaehlernummer, Datum, Zaehlernummer_in_khw und Kommentar.
 CREATE TABLE IF NOT EXISTS strom (
-    kundenid VARCHAR(255),
-    zaehlernummer VARCHAR(255),
-    datum DATE,
-    zaehlerstand_in_kwh FLOAT,
-    kommentar VARCHAR(255)
+    kundenid VARCHAR(255) NOT NULL,
+    zaehlernummer VARCHAR(255) NOT NULL UNIQUE,
+    datum DATE NOT NULL,
+    zaehlerstand_in_kwh FLOAT NOT NULL,
+    kommentar VARCHAR(255) NOT NULL
 );
 --Erstellt die Tabelle Kunde mit den Spalten UUID, Anrede, Nachname, Vorname und Geburtsdatum.
 CREATE TABLE IF NOT EXISTS kunde (
-    uuid VARCHAR(255),
-    anrede VARCHAR(1),
-    nachname VARCHAR(255),
-    vorname VARCHAR(255),
-    geburtsdatum DATE
+    uuid VARCHAR(255) NOT NULL UNIQUE,
+    anrede VARCHAR(1) NOT NULL,
+    nachname VARCHAR(255) NOT NULL,
+    vorname VARCHAR(255) NOT NULL,
+    geburtsdatum DATE NOT NULL
 );
