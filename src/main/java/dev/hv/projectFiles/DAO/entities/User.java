e2241e24711ffe386974178639e1c4147e8bd115
Raphael Rauch <raphael.rauch123@gmail.com>
2025-01-20 10:49:04 +0100

    use of hibernate validator instead of only jakarta

--- src/main/java/dev/hv/projectFiles/DAO/entities/User.java
+++ src/main/java/dev/hv/projectFiles/DAO/entities/User.java
@@ -1,32 +1,39 @@
 package dev.hv.projectFiles.DAO.entities;
 
 import dev.hv.model.ICustomer;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.UUID;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 public class User implements ICustomer {
 
-    @NotNull(message = "First name cannot be null")
-    @Size(min = 1, message = "First name cannot be empty")
+    @NotNull(message = "First name cannot be empty")  // Hibernate-specific annotation
+    @Length(min = 1, max = 50, message = "First name must be between 1 and 50 characters")  // Hibernate-specific annotation
+    @JsonProperty("first_name")
     String firstName;
 
-    @NotNull(message = "Last name cannot be null")
-    @Size(min = 1, message = "Last name cannot be empty")
+    @NotNull(message = "Last name cannot be empty")  // Hibernate-specific annotation
+    @Length(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")  // Hibernate-specific annotation
+    @JsonProperty("last_name")
     String lastName;
 
-    @NotNull(message = "Birthdate cannot be null")
-    @Past(message = "Birthdate must be in the past")
+    @NotNull(message = "Birthdate cannot be empty")  // Hibernate-specific annotation
+    @JsonProperty("birth_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     LocalDate birthdate;
 
-    @NotNull(message = "Gender cannot be null")
+    @NotNull(message = "Gender cannot be empty")  // Hibernate-specific annotation
+    @JsonProperty("gender")
     Gender gender;
 
-    @NotNull(message = "ID cannot be null")
-    UUID id;
+    @NotNull(message = "ID cannot be empty")  // Hibernate-specific annotation
+    @UUID(message = "ID must be a valid UUID")  // Hibernate-specific annotation
+    @JsonProperty("id")
+    java.util.UUID id;
 
     @Override
     public LocalDate getBirthDate() {
@@ -69,12 +76,12 @@ public class User implements ICustomer {
     }
 
     @Override
-    public UUID getId() {
+    public java.util.UUID getId() {
         return id;
     }
-
+    
     @Override
-    public void setId(UUID id) {
+    public void setId(java.util.UUID id) {
         this.id = id;
     }
 }
