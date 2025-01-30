b949698258c0ca8c82b4ee91c8acc9967266eb50
Raphael Rauch <raphael.rauch123@gmail.com>
2025-01-30 12:26:57 +0100

    changes in sql files for adopting to uuid changes in db

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -1,6 +1,6 @@
 --Erstellt die Tabelle Heizung mit den Spalten KundenID, Zaehlernummer, Datum, Zaehlernummer_in_mhw und Kommentar.
 CREATE TABLE IF NOT EXISTS heizung (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(255) PRIMARY KEY,
     kundenid VARCHAR(255),
     zaehlernummer VARCHAR(255) NOT NULL,
     datum DATE NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS heizung (
 );
 --Erstellt die Tabelle Wasser mit den Spalten KundenID, Zaehlernummer, Datum, Zaehlernummer_in_m³ und Kommentar.
 CREATE TABLE IF NOT EXISTS wasser (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(255) PRIMARY KEY,
     kundenid VARCHAR(255),
     zaehlernummer VARCHAR(255) NOT NULL,
     datum DATE NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS wasser (
 );
 --Erstellt die Tabelle Strom mit den Spalten KundenID, Zaehlernummer, Datum, Zaehlernummer_in_khw und Kommentar.
 CREATE TABLE IF NOT EXISTS strom (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(255) PRIMARY KEY,
     kundenid VARCHAR(255),
     zaehlernummer VARCHAR(255) NOT NULL,
     datum DATE NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS strom (
 );
 --Erstellt die Tabelle Kunde mit den Spalten UUID, Anrede, Nachname, Vorname und Geburtsdatum.
 CREATE TABLE IF NOT EXISTS kunde (
-    uuid VARCHAR(255) NOT NULL UNIQUE,
+    uuid VARCHAR(255) PRIMARY KEY,
     anrede VARCHAR(1) NOT NULL,
     nachname VARCHAR(255) NOT NULL,
     vorname VARCHAR(255) NOT NULL,
