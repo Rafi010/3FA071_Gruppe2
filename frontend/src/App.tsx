665bb50aa3d58845e9939e01311969c2a6d5312d
Raphael Rauch <raphael.rauch123@gmail.com>
2025-03-17 12:59:42 +0100

    change App.tsx to fit needs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/data';
+import MyComponent from './components/CustomerGrid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <h1>People List</h1>
         <MyComponent /> {/* Use MyComponent to display the list */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
