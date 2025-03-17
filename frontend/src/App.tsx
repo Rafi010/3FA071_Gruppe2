f0daec9c593e07c3db239516b4bb144c180853de
Raphael Rauch <raphael.rauch123@gmail.com>
2025-03-17 09:01:50 +0100

    added fetch in frontend to fetch one or more customers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/data';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1>People List</h1>
+        <MyComponent /> {/* Use MyComponent to display the list */}
         <a
           className="App-link"
           href="https://reactjs.org"
