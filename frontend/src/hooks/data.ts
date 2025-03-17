77746f6578a76de6bc0776f63b755e556eeb1271
Raphael Rauch <raphael.rauch123@gmail.com>
2025-03-17 13:01:47 +0100

    data requst method and put data logic in hooks

---
+++ frontend/src/hooks/data.ts
@@ -0,0 +1,36 @@
+// hooks/useGetData.ts
+import { useState, useEffect } from 'react';
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  gender: string;
+  id: string;
+  birthDate: number[] | null;
+}
+
+export enum DataType {
+  Customers = 'customers',
+  Readings = 'readings',
+}
+
+export const useGetData = (dataType: DataType) => {
+  const [data, setData] = useState<Person[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/${dataType}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, 
+  []);
+
+  return { data, loading };
+};
